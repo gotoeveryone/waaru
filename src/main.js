001a0b2655c453ca6e8fab9e3241f06dcdd846e7
Kazuki_Kamizuru <k.amago0721@gmail.com>
2019-04-28 13:20:57 +0900

    add firebase config with dotenv

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
+import firebase from 'firebase';
+import Vue from 'vue';
+import App from './App.vue';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+// Firebase の設定
+const config = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+};
+firebase.initializeApp(config);
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
