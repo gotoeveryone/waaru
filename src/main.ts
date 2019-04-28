959866a3b1f433195d41a9a65ef70f95155ae3ec
Kazuki_Kamizuru <k.amago0721@gmail.com>
2019-04-28 14:12:59 +0900

    add: firebase config

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import firebase from "firebase";
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
@@ -5,8 +6,19 @@ import store from "./store";
 
 Vue.config.productionTip = false;
 
+// Firebase の設定
+const config = {
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID
+};
+firebase.initializeApp(config);
+
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h: Function) => h(App)
 }).$mount("#app");
